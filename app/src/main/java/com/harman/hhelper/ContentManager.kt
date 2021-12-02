6642f4ced0966210d2164313b219b52c1fbef1cb
anof1r <Deniska20121261999>
2021-12-02 03:29:59 +0300

    added ContentManager

--- app/src/main/java/com/harman/hhelper/ContentManager.kt
+++ app/src/main/java/com/harman/hhelper/ContentManager.kt
@@ -1,4 +1,23 @@
 package com.harman.hhelper
 
-class ContentManager {
+import android.content.Intent
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class ContentManager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_manager)
+
+        val web = findViewById<WebView>(R.id.webView)
+        var url = intent.getStringExtra("url")
+
+        if (url != null) {
+            web.webViewClient
+            web.settings.javaScriptEnabled
+            web.loadUrl(url)
+        }
+    }
 }
--- app/src/main/java/com/harman/hhelper/MainActivity.kt
+++ app/src/main/java/com/harman/hhelper/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.harman.hhelper
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,12 +19,12 @@ import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
 
-    var adapter : RcViewAdapter?= null
-    var mcResponse : MainContentResponse = MainContentResponse()
-    var information : Information = Information()
-    var contentArray : ArrayList<MainContent> = ArrayList()
+    private var adapter : RcViewAdapter?= null
+    private var mcResponse : MainContentResponse = MainContentResponse()
+    private var information : Information = Information()
+    private var contentArray : ArrayList<MainContent> = ArrayList()
     private val auth : FirebaseAuth = FirebaseAuth.getInstance()
-    lateinit var inf : InfoResponse
+    private lateinit var informationResponse : InfoResponse
 
     @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +33,7 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
         val nav_view = findViewById<NavigationView>(R.id.nav_view)
         nav_view.setNavigationItemSelectedListener(this)
 
-
+        //webView = WebView(this)
         val rcView : RecyclerView = findViewById(R.id.rcView)
 
         val list = ArrayList<MainContent>()
@@ -44,15 +43,16 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
         adapter = RcViewAdapter(list,this)
         rcView.adapter = adapter
 
+
+
         GlobalScope.launch {
                 contentArray = mcResponse.getMainContent()
                 adapter?.updateAdapter(contentArray)
-                inf = information.getMainContent()
-
+                informationResponse = information.getMainContent()
+            //val user = findViewById<TextView>(R.id.header_info)
+            //user.text = auth.currentUser?.email
         }
-
     }
-
     override fun onStart(){
         super.onStart()
         if(auth.currentUser == null){
@@ -60,6 +60,7 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
         }
     }
 
+    @SuppressLint("ResourceType")
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.id_course -> {
@@ -71,16 +72,23 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
                 intent.putExtra("links",information.getLinks())
                 intent.putExtra("literature",information.getLinksL())
                 intent.putExtra("schedule",information.getSchedule())
+                //переделать на NestedScrollView
                 startActivity(intent)
             }
-
             R.id.id_contacts ->{
 
             }
-            R.id.id_settings ->{
+            R.id.night_mode ->{
+
+            }
+            R.id.id_sign_out ->{
                 auth.signOut()
-                //переделать на NestedScrollView
                 startActivity(Intent(this@MainActivity,LoginActivity::class.java))
+            }
+            R.id.id_content_manager -> {
+                val intent = Intent(this@MainActivity,ContentManager::class.java)
+                intent.putExtra("url","http://192.168.0.4:8080/web_view")
+                startActivity(intent)
             }
         }
         val drawerLayout  = findViewById<DrawerLayout>(R.id.drawer_layout)
@@ -96,7 +104,7 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
         return listItemArray
     }
      */
-
+    /*
     fun getImageId(imageArrayId: Int): IntArray{
         val tArray: TypedArray = resources.obtainTypedArray(imageArrayId)
         val count = tArray.length()
@@ -107,4 +115,6 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
         tArray.recycle()
         return ids
     }
+
+     */
 }
