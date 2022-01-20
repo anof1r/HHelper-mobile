9699e9ec76a398b7fb949720fd8853cc8eb69db0
anof1r <Deniska20121261999>
2022-01-21 00:11:08 +0300

    .

--- app/src/main/java/com/harman/hhelper/ApiRequests.kt
+++ app/src/main/java/com/harman/hhelper/ApiRequests.kt
@@ -1,10 +1,22 @@
 package com.harman.hhelper
 
 import com.harman.hhelper.api.LectureJson
+import com.harman.hhelper.api.LectureJsonItem
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.Response
+import retrofit2.http.*
 
 interface ApiRequests {
+
     @GET("/main_content")
     fun getLectures(): Call<LectureJson>
+
+    @FormUrlEncoded
+    @POST("/addLecture")
+    fun postLecture(@Field("content") content: String,
+                    @Field("date") date: String,
+                    @Field("hw") homeWork: String,
+                    @Field("id") id: Int,
+                    @Field("imgId") ImageId: String,
+                    @Field("title") title: String) : Call<LectureJson>
 }
--- app/src/main/java/com/harman/hhelper/MainActivity.kt
+++ app/src/main/java/com/harman/hhelper/MainActivity.kt
@@ -1,52 +1,49 @@
 package com.harman.hhelper
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.harman.hhelper.api.LectureJson
+import com.harman.hhelper.api.LectureJsonItem
 import com.harman.hhelper.information_response.InfoResponse
 import com.harman.hhelper.information_response.Information
-import com.harman.hhelper.main_content_jsonresponse.MainContent
-import com.harman.hhelper.main_content_jsonresponse.MainContentResponse
 import kotlinx.coroutines.*
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.Exception
 
 const val BASE_URL = "http://192.168.0.6:8080"
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var adapter: RcViewAdapter? = null
-    private var mcResponse: MainContentResponse = MainContentResponse()
+    //private var mcResponse: MainContentResponse = MainContentResponse()
     private var information: Information = Information()
     private lateinit var contentArray: LectureJson
     private lateinit var auth: FirebaseAuth
     private lateinit var fStore: FirebaseFirestore
     private lateinit var informationResponse: InfoResponse
     private var isAdminCheck: Boolean = false
-    private var TAG = "MainActivity"
+
 
     @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +54,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val rcView: RecyclerView = findViewById(R.id.rcView)
 
         val list = LectureJson()
-        val loadingDialog = LoadingDialog(this)
+        //val loadingDialog = LoadingDialog(this)
         val refresh = findViewById<SwipeRefreshLayout>(R.id.swipe_refresh)
 
         val fab: View = findViewById(R.id.fab_main)
@@ -72,21 +69,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         auth = FirebaseAuth.getInstance()
         fStore = FirebaseFirestore.getInstance()
-        loadingDialog.startLoadingDialog()
+        //loadingDialog.startLoadingDialog()
         GlobalScope.launch {
-
                 contentArray = getCurrentData()
                 informationResponse = information.getMainContent()
-            runOnUiThread() {
+            runOnUiThread {
                 adapter?.updateAdapter(contentArray)
-                loadingDialog.dismissDialog()
+                //loadingDialog.dismissDialog()
             }
         }
         refresh.setOnRefreshListener {
             GlobalScope.launch {
                 contentArray.clear()
                 contentArray = getCurrentData()
-
                 runOnUiThread {
                     adapter?.updateAdapter(contentArray)
                     refresh.isRefreshing = false
@@ -109,6 +104,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         return data
     }
+
+    @DelicateCoroutinesApi
+    private suspend fun postCurrentData(item: LectureJsonItem) {
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiRequests::class.java)
+        api.postLecture(item.content,item.date,item.homeWork,item.id,item.imageId,item.title).awaitResponse()
+    }
+
 
     @SuppressLint("ResourceType")
     override fun onStart() {
@@ -134,8 +140,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 startActivity(intent)
             }
             R.id.id_contacts -> {
-                //val frag = Fragment(R.layout.fragment_info_f_fragment)
-                //loadFragment(frag)
             }
             R.id.night_mode -> {
             }
@@ -144,19 +148,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 startActivity(Intent(this@MainActivity, LoginActivity::class.java))
             }
             R.id.id_content_manager -> {
-                if (isAdminCheck) {
-                    Toast.makeText(this, "Welcome Admin!", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this@MainActivity, ContentManager::class.java)
-                    //intent.putExtra("url", "http://95.79.178.246:8080/web_view")
-                    intent.putExtra("url", "http://192.168.0.6:8080/web_view")
-                    startActivity(intent)
-                } else {
-                    Toast.makeText(
-                        this,
-                        "You're not allowed to use ContentManager",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
             }
         }
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
@@ -177,31 +168,40 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return isAdminCheck
     }
 
+    @DelicateCoroutinesApi
     private fun addInfo() {
         val inflater = LayoutInflater.from(this)
         val v = inflater.inflate(R.layout.add_item, null)
 
-        //val userName = v.findViewById<EditText>(R.id.userName)
-        //val userNo = v.findViewById<EditText>(R.id.userNo)
+        val addTitle = v.findViewById<EditText>(R.id.addTitle)
+        val addLectureInfo = v.findViewById<EditText>(R.id.addContent)
+        val addImg = v.findViewById<EditText>(R.id.addImage)
+        val addDate = v.findViewById<EditText>(R.id.addDate)
+        val addHw = v.findViewById<EditText>(R.id.addHomeWork)
+        //val addId = v.findViewById<EditText>(R.id.addId)
 
         val addDialog = AlertDialog.Builder(this)
 
         addDialog.setView(v)
         addDialog.setPositiveButton("Ok") { dialog, _ ->
-            //val names = userName.text.toString()
-            //val number = userNo.text.toString()
-            //userList.add(UserData("Name: $names","Mobile No. : $number"))
-            //userAdapter.notifyDataSetChanged()
+            val content = addLectureInfo.text.toString()
+            val date = addDate.text.toString()
+            val homeWork = addHw.text.toString()
+            val imageId = addImg.text.toString()
+            val title = addTitle.text.toString()
+            //val id = addId.text.toString().toInt()
+            val item = LectureJsonItem(content,date,homeWork,1,imageId,title)
+            GlobalScope.launch {
+                postCurrentData(item)
+            }
             Toast.makeText(this, "Adding User Information Success", Toast.LENGTH_SHORT).show()
             dialog.dismiss()
         }
         addDialog.setNegativeButton("Cancel") { dialog, _ ->
             dialog.dismiss()
             Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show()
-
         }
         addDialog.create()
         addDialog.show()
     }
-
 }
